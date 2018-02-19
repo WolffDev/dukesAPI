8bdfe21b53b32371148bf372b80b2c27e88a1a62
David Wolff <info@davidwolff.dk>
2018-02-19 15:06:05 +0100

    testing connection

--- sandbox/testMysql.js
+++ sandbox/testMysql.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-	socketPath: '/var/run/mysqld/mysqld.sock',
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABSASE 
-});
+const connection = async function() {
+	await mysql.createConnection({
+		socketPath: '/var/run/mysqld/mysqld.sock',
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABSASE 
+	});
+};
 async function main() {
 	return [rows, fields] = await connection.execute('SELECT text FROM app_posts WHERE user_id = ?', [1]);
 }

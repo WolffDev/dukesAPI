218bc5e448e04a6befcd4c207b24cf46762b265a
David Wolff <info@davidwolff.dk>
2018-03-25 22:26:11 +0200

    changed some logic for deleting posts

--- server/middleware/post/deletePostMiddleware.js
+++ server/middleware/post/deletePostMiddleware.js
@@ -3,13 +3,15 @@ const { getAutherFromPost, getAuthLevelFromPost } = require('../../api/forum/pos
 module.exports = (req, res, next) => {
 	getAutherFromPost(req.params.id)
 		.then( result => {
-			if(result[0].user_id === +req.user_id) return next()
+			if(result[0].user_id === +req.user_id) {
+				return next();
+			}
 		})
 		.catch(err => next(err))
 
 	getAuthLevelFromPost(req.params.id)
 		.then( result => {
-			if(result[0].auth_level <= +req.auth_level) return next();
+			if(result[0].auth_level <= +req.auth_level && req.auth_level >= 3) return next();
 			next({
 				type: 'error',
 				name: 'DeletePostInvalidAuthLevel',
